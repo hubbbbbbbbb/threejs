63af5fd6836438795ad96e798472f1e116637d27
hubbbbbbbbb <hubyu9234@gmail.com>
2023-10-16 00:11:10 -0700

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
     color: "#EFBD48",
     isLogoTexture: true,
     isFullTexture: false,
-    logoDecal: "./assets/threejs.png",
-    fullDecal: "./assets/threejs.png",
+    logoDecal: "/threejs.png",
+    fullDecal: "/threejs.png",
 })
 
 export default state;
